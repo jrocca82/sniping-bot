570a802b36ae94054aae2f226845208d54c4b510
Jo <loluvulol@gmail.com>
2022-06-22 15:39:33 +1000

    Update project

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,30 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-pragma abicoder v2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is IERC20, ERC20, Ownable {
-    uint8 private immutable _decimals;
-
+contract Token is ERC20 {
     constructor(
-        string memory name_,
-        string memory symbol_,
-        uint8 decimals_
-    ) ERC20(name_, symbol_) {
-        _decimals = decimals_;
-    }
-
-    function mint(address account, uint256 amount) external onlyOwner {
-        _mint(account, amount);
-    }
-
-    function burn(address account, uint256 amount) external onlyOwner {
-        _burn(account, amount);
-    }
-
-    function decimals() public view override returns (uint8) {
-        return _decimals;
+        string memory _name,
+        string memory _symbol,
+        uint256 _initialSupply
+    ) ERC20(_name, _symbol) {
+        _mint(msg.sender, _initialSupply);
     }
 }
